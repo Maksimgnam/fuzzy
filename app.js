210a9d0e7cce71160410f9dbd779ce511d6e0e29
Maksimgnam <107640396+Maksimgnam@users.noreply.github.com>
2022-10-09 09:16:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+re1.onclick = () =>{
+    plu1.style.display = 'flex'
+}
+re2.onclick = () =>{
+    plu2.style.display = 'flex'
+}
+re3.onclick = () =>{
+    plu3.style.display = 'flex'
+}
+re4.onclick = () =>{
+    plu4.style.display = 'flex'
+}
+re5.onclick = () =>{
+    plu5.style.display = 'flex'
+}
+re6.onclick = () =>{
+    plu6.style.display = 'flex'
+}
+re7.onclick = () =>{
+    plu7.style.display = 'flex'
+}
+re8.onclick = () =>{
+    plu8.style.display = 'flex'
+}
+re9.onclick = () =>{
+    plu9.style.display = 'flex'
+}
+
+rer1.onclick = () =>{
+    plur1.style.display = 'flex'
+}
+rer2.onclick = () =>{
+    plur2.style.display = 'flex'
+}
+rer3.onclick = () =>{
+    plur3.style.display = 'flex'
+}
+rer4.onclick = () =>{
+    plur4.style.display = 'flex'
+}
+rer5.onclick = () =>{
+    plur5.style.display = 'flex'
+}
+rer6.onclick = () =>{
+    plur6.style.display = 'flex'
+}
+rer7.onclick = () =>{
+    plur7.style.display = 'flex'
+}
+rer8.onclick = () =>{
+    plur8.style.display = 'flex'
+}
+rer9.onclick = () =>{
+    plur9.style.display = 'flex'
+}
+
+rere1.onclick = () =>{
+    plure1.style.display = 'flex'
+}
+rere2.onclick = () =>{
+    plure2.style.display = 'flex'
+}
+rere3.onclick = () =>{
+    plure3.style.display = 'flex'
+}
+rere4.onclick = () =>{
+    pluer4.style.display = 'flex'
+}
+rere5.onclick = () =>{
+    plure5.style.display = 'flex'
+}
+rere6.onclick = () =>{
+    plure6.style.display = 'flex'
+}
+rere7.onclick = () =>{
+    plure7.style.display = 'flex'
+}
+rere8.onclick = () =>{
+    plure8.style.display = 'flex'
+}
+rere9.onclick = () =>{
+    plure9.style.display = 'flex'
+}
+rerer1.onclick = () =>{
+    plurer1.style.display = 'flex'
+}
+rerer2.onclick = () =>{
+    plurer2.style.display = 'flex'
+}
+rerer3.onclick = () =>{
+    plurer3.style.display = 'flex'
+}
+rerer4.onclick = () =>{
+    pluerer4.style.display = 'flex'
+}
+rerer5.onclick = () =>{
+    plurer5.style.display = 'flex'
+}
+rerer6.onclick = () =>{
+    plurer6.style.display = 'flex'
+}
+rerer7.onclick = () =>{
+    plurer7.style.display = 'flex'
+}
+rerer8.onclick = () =>{
+    plurer8.style.display = 'flex'
+}
+rerer9.onclick = () =>{
+    plurer9.style.display = 'flex'
+}
+
+
+log.onclick = ()=>{
+    cont.style.display = 'none'
+    left.style.display = 'block'
+    right.style.display = 'block'
+    text.innerText =  inp1.value;
+    mail.innerText = inp1.value + '@gmail.com'
+
+}
+back.onclick = ()=>{
+    cont.style.display = 'block'
+    left.style.display = 'none'
+    right.style.display = 'none'
+   
+
+}
+
+
+
